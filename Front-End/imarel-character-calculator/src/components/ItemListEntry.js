35a06aed86e8388762ec12cc4b892970533e3336
Tanner Hawkins <TTHawkins@student.fullsail.edu>
2022-06-23 12:00:10 -0400

    item list go brr

--- Front-End/imarel-character-calculator/src/components/ItemListEntry.js
+++ Front-End/imarel-character-calculator/src/components/ItemListEntry.js
@@ -4,17 +4,42 @@ const ItemListEntry = (props) => {
 
     const { globalState, setGlobalState } = React.useContext(GlobalContext);
 
+    const [itemListEntryState, setItemListEntryState] = useState({ enabled: false, collapsed: true });
+
+    const handleCheckbox = (event) => {
+        setItemListEntryState((previous) => ({
+            ...previous,
+            enabled: !previous.enabled
+        }))
+    }
+
+    const handleCollapse = (event) => {
+        setItemListEntryState((previous) => ({
+            ...previous,
+            collapsed: !previous.collapsed
+        }))
+    }
+
     return (
         <div>
-            <b>{props.itemKey + ": "}</b>
-            {Object.keys(globalState.items[props.itemKey].stats).map((statName) => (
-                <>{globalState.items[props.itemKey].stats[statName].base}% base {statName}, {globalState.items[props.itemKey].stats[statName].bonus}% bonus {statName}; </>
-            ))}
-            <input type="button" onClick={()=>{
-                delete globalState.items[props.itemKey];
-                setGlobalState((previous) => ({...previous}))
-            }} value="Delete item"/>
+            <div style={{ display: "flex", flexFlow: "row nowrap", justifyContent: "space-between" }} onClick={handleCollapse}>
+                <div>
+                    <input id="itemCheckbox" name="itemCheckbox" type={"checkbox"} onChange={handleCheckbox} checked={itemListEntryState.enabled} />
+                    <b>{props.itemKey}</b>
+                </div>
+                <input type="button" onClick={() => {
+                    delete globalState.items[props.itemKey];
+                    setGlobalState((previous) => ({ ...previous }))
+                }} value="Delete item" />
+            </div>
+            {
+                ((itemListEntryState && itemListEntryState.collapsed == false) ? Object.keys(globalState.items[props.itemKey].stats).map((statName) => (
+                    <>{globalState.items[props.itemKey].stats[statName].base}% base {statName}, {globalState.items[props.itemKey].stats[statName].bonus}% bonus {statName}; </>
+                )) : [])
+            }
         </div>
+
+
     );
 }
 
