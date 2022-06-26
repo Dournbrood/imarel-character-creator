3d7419402a2cae86d48f365bd4b922e229e6845a
Tanner Hawkins <TTHawkins@student.fullsail.edu>
2022-06-26 11:08:00 -0400

    implements item disabling

--- Front-End/imarel-character-calculator/src/components/ItemList.js
+++ Front-End/imarel-character-calculator/src/components/ItemList.js
@@ -6,7 +6,7 @@ const ItemList = (props) => {
     const { globalState, setGlobalState } = React.useContext(GlobalContext);
 
     return (
-        <div style={{display: "flex", flexFlow: "column nowrap"}}>
+        <div style={{display: "flex", flexFlow: "column nowrap", gap: "8px", margin: "8px 0"}}>
             {((globalState && globalState.items) ? Object.keys(globalState.items).map((key) => (
                 <ItemListEntry itemKey={key}/>
             )) : [])}
--- Front-End/imarel-character-calculator/src/components/ItemListEntry.js
+++ Front-End/imarel-character-calculator/src/components/ItemListEntry.js
@@ -7,9 +7,15 @@ const ItemListEntry = (props) => {
     const [itemListEntryState, setItemListEntryState] = useState({ enabled: false, collapsed: true });
 
     const handleCheckbox = (event) => {
-        setItemListEntryState((previous) => ({
-            ...previous,
-            enabled: !previous.enabled
+        setGlobalState((previous) => ({
+            ...(previous),
+            items: {
+                ...(previous && previous.items),
+                [props.itemKey]: {
+                    ...(previous && previous.items && previous.items[props.itemKey]),
+                    enabled: ( previous && previous.items && previous.items[props.itemKey] && previous.items[props.itemKey].enabled !== undefined ? !previous.items[props.itemKey].enabled : true )
+                }
+            }
         }))
     }
 
@@ -20,27 +26,32 @@ const ItemListEntry = (props) => {
         }))
     }
 
-    return (
-        <div>
-            <div style={{ display: "flex", flexFlow: "row nowrap", justifyContent: "space-between" }} onClick={handleCollapse}>
-                <div>
-                    <input id="itemCheckbox" name="itemCheckbox" type={"checkbox"} onChange={handleCheckbox} checked={itemListEntryState.enabled} />
-                    <b>{props.itemKey}</b>
+    try {
+        return (
+            <div>
+                <div style={{ display: "flex", flexFlow: "row nowrap", justifyContent: "space-between" }} onClick={handleCollapse}>
+                    <div>
+                        <input id="itemCheckbox" name="itemCheckbox" type={"checkbox"} onChange={handleCheckbox} checked={globalState.items[props.itemKey].enabled} />
+                        <b>{props.itemKey}</b>
+                    </div>
+                    <input type="button" onClick={() => {
+                        delete globalState.items[props.itemKey];
+                        setGlobalState((previous) => ({ ...previous }))
+                    }} value="Delete item" />
                 </div>
-                <input type="button" onClick={() => {
-                    delete globalState.items[props.itemKey];
-                    setGlobalState((previous) => ({ ...previous }))
-                }} value="Delete item" />
+                {
+                    ((itemListEntryState && itemListEntryState.collapsed === false) ? Object.keys(globalState.items[props.itemKey].stats).map((statName) => (
+                        <>{globalState.items[props.itemKey].stats[statName].base}% base {statName}, {globalState.items[props.itemKey].stats[statName].bonus}% bonus {statName}; </>
+                    )) : [])
+                }
             </div>
-            {
-                ((itemListEntryState && itemListEntryState.collapsed == false) ? Object.keys(globalState.items[props.itemKey].stats).map((statName) => (
-                    <>{globalState.items[props.itemKey].stats[statName].base}% base {statName}, {globalState.items[props.itemKey].stats[statName].bonus}% bonus {statName}; </>
-                )) : [])
-            }
-        </div>
-
-
-    );
+    
+    
+        );
+    } catch (error) {
+        console.log(error);
+    }
+  
 }
 
 export default ItemListEntry;
